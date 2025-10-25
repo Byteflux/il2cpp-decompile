6d0c40fac4dc312255a8cf9dcdc7553f37848b45
Matthew Harris <byte@byteflux.net>
2025-10-25 17:37:25 -0400

    Renames to indicate everything other than main() is internal

--- il2cpp_decompile.py
+++ il2cpp_decompile.py
@@ -17,14 +17,14 @@ APPS_DIR = DATA_DIR / "apps"
 
 def main() -> None:
     if Path(sys.prefix) != VENV_DIR:
-        bootstrap()
+        _bootstrap()
         return
 
     _load_dotenv()
 
     args = sys.argv[1:]
     if not args:
-        run_ghidra()
+        _run_ghidra()
         return
 
     game_dir = Path(args[0])
@@ -56,7 +56,7 @@ def main() -> None:
 
     project_file = work_dir / f"{game_dir.name}.gpr"
     if project_file.exists():
-        run_ghidra([project_file])
+        _run_ghidra([project_file])
         return
 
     for file in [game_assembly_file, global_metadata_file]:
@@ -67,10 +67,10 @@ def main() -> None:
     game_assembly_file = work_dir / game_assembly_file.relative_to(game_dir.parent)
     global_metadata_file = work_dir / global_metadata_file.relative_to(game_dir.parent)
 
-    run_il2cpp_dumper([game_assembly_file, global_metadata_file, work_dir])
-    run_il2cpp_header_to_ghidra(work_dir)
+    _run_il2cppdumper([game_assembly_file, global_metadata_file, work_dir])
+    _run_il2cppdumper_header_to_ghidra(work_dir)
 
-    run_ghidra(
+    _run_ghidra(
         [
             "--headless",
             work_dir,
@@ -89,23 +89,23 @@ def main() -> None:
         ]
     )
 
-    run_ghidra([project_file])
+    _run_ghidra([project_file])
 
 
-def run_il2cpp_dumper(args: list[str | os.PathLike]) -> None:
-    il2cpp_dumper_path = APPS_DIR / "Il2CppDumper/Il2CppDumper.exe"
-    if not il2cpp_dumper_path.exists():
+def _run_il2cppdumper(args: list[str | os.PathLike]) -> None:
+    il2cppdumper_path = APPS_DIR / "Il2CppDumper/Il2CppDumper.exe"
+    if not il2cppdumper_path.exists():
         il2cppdumper_download_url = os.getenv("IL2CPPDECOMPILE_DOWNLOAD_URL_IL2CPPDUMPER")
         if il2cppdumper_download_url is None:
             print("Missing download URL for Il2CppDumper")
             sys.exit(1)
 
-        download_and_extract(il2cppdumper_download_url, "Il2CppDumper")
-        if not il2cpp_dumper_path.exists():
+        _download_and_extract(il2cppdumper_download_url, "Il2CppDumper")
+        if not il2cppdumper_path.exists():
             print("Could not find Il2CppDumper.exe")
             sys.exit(1)
 
-    config_file = il2cpp_dumper_path.parent / "config.json"
+    config_file = il2cppdumper_path.parent / "config.json"
     config = {}
     if config_file.exists():
         with open(config_file) as f:
@@ -115,19 +115,19 @@ def run_il2cpp_dumper(args: list[str | os.PathLike]) -> None:
         with open(config_file, "w") as f:
             json.dump(config, f)
 
-    result = subprocess.run([il2cpp_dumper_path, *args])
+    result = subprocess.run([il2cppdumper_path, *args])
     if result.returncode > 0:
         sys.exit(result.returncode)
 
 
-def run_il2cpp_header_to_ghidra(work_dir) -> None:
+def _run_il2cppdumper_header_to_ghidra(work_dir) -> None:
     script_path = APPS_DIR / "Il2CppDumper/il2cpp_header_to_ghidra.py"
     result = subprocess.run([sys.executable, script_path], cwd=work_dir)
     if result.returncode > 0:
         sys.exit(result.returncode)
 
 
-def run_ghidra(args: list[str | os.PathLike] = []) -> None:
+def _run_ghidra(args: list[str | os.PathLike] = []) -> None:
     java_glob_pattern = "jdk-*/bin/java.exe"
     java_path = next(APPS_DIR.glob(java_glob_pattern), None)
     if java_path is None:
@@ -136,7 +136,7 @@ def run_ghidra(args: list[str | os.PathLike] = []) -> None:
             print("Missing download URL for JDK")
             sys.exit(1)
 
-        download_and_extract(jdk_download_url)
+        _download_and_extract(jdk_download_url)
         java_path = next(APPS_DIR.glob(java_glob_pattern), None)
         if java_path is None:
             print("Could not find java.exe")
@@ -150,7 +150,7 @@ def run_ghidra(args: list[str | os.PathLike] = []) -> None:
             print("Missing download URL for Ghidra")
             sys.exit(1)
 
-        download_and_extract(ghidra_download_url)
+        _download_and_extract(ghidra_download_url)
         ghidra_path = next(APPS_DIR.glob(ghidra_glob_pattern), None)
         if ghidra_path is None:
             print("Could not find pyghidraRun.bat")
@@ -164,7 +164,7 @@ def run_ghidra(args: list[str | os.PathLike] = []) -> None:
         sys.exit(result.returncode)
 
 
-def download_and_extract(url: str, name: Optional[str] = None) -> None:
+def _download_and_extract(url: str, name: Optional[str] = None) -> None:
     import requests
 
     with TemporaryFile() as f:
@@ -181,7 +181,7 @@ def download_and_extract(url: str, name: Optional[str] = None) -> None:
                 z.extractall(APPS_DIR)
 
 
-def bootstrap() -> None:
+def _bootstrap() -> None:
     venv_python = VENV_DIR / "Scripts/python.exe"
     if not venv_python.exists():
         shutil.rmtree(VENV_DIR, ignore_errors=True)
